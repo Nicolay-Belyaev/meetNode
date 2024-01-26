21221a7c5973562ad8ecb9aa1b590086eafce0c2
NicolayBelyaev <nickb2147@gmail.com>
2024-01-27 00:16:10 +0300

    homework_03

--- express/index.js
+++ express/index.js
@@ -1,28 +1,46 @@
 const express = require(`express`);
 const fs = require(`fs`);
-const path = require(`path`);
 
 const app = express();
 const port = 3000;
 const countersPath = "counters.json";
 
+app.use((req, res, next) => {
+    const countersObj = JSON.parse(fs.readFileSync(countersPath, 'utf8'))
+    switch (req.url) {
+        case "/":
+            countersObj.mainPageCounter += 1;
+            break;
+        case "/about":
+            countersObj.aboutPageCounter += 1;
+            break;
+    }
+    fs.writeFileSync(countersPath, JSON.stringify(countersObj));
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send("Welcome to my Express HTTP Server!")
+    const countersObj = JSON.parse(fs.readFileSync(countersPath, 'utf8'))
+    res.send(`<h1>Welcome to my Express HTTP Server!</h1>` +
+                   `<p>Main page visits counter: ${countersObj.mainPageCounter}</p>` +
+                   `<a href="/about">About page</a>`)
 });
 
 app.get('/about', (req, res) => {
-
+    const countersObj = JSON.parse(fs.readFileSync(countersPath, 'utf8'))
+    res.send(`<h1>About page!</h1>` +
+                   `<p>About page visits counter: ${countersObj.aboutPageCounter}</p>`+
+                   `<a href="/">Main page</a>`)
 })
 app.listen(port, () => {
     if(!fs.existsSync(countersPath)) {
         try {
-            fs.writeFileSync(countersPath, `{\n"mainPageCounter": 0,\n "aboutPageCounter": 0\n}`)
+            fs.writeFileSync(countersPath, `{\n"mainPageCounter": 0,\n"aboutPageCounter": 0\n}`)
             console.log("Counters log file created successfully")
         } catch (err) {
             console.log("Cannot create log file!");
             console.log(err);
         }
     }
-
     console.log(`Sever successfully started on port ${port}`)
 })
